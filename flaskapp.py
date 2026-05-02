fca67484ac14d73ca3277559e773ee5524acaa51
Najmo <najmo.mahamed@drake.edu>
2026-05-02 14:08:20 -0500

    new updates

--- flaskapp.py
+++ flaskapp.py
@@ -1,60 +1,86 @@
 # author: Najmo
 # description: Flask example using redirect, url_for, and flash
 # credit: the template html files were constructed with the help of ChatGPT
-
+# description: Country Explorer Flask App using MySQL (world database)
 from flask import Flask, render_template, request, redirect, url_for, flash
 from dbCode import *
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = "your_secret_key"
 
 
+# -------------------------
+# HOME PAGE
+# -------------------------
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-# shows countries 
+# -------------------------
+# SHOW ALL COUNTRIES
+# -------------------------
 @app.route('/countries')
 def countries():
-    query = "SELECT Name, Continent, Population FROM Country LIMIT 20;"
-    data = execute_query(query)
-    return render_template('display_users.html', countries=data)
+    try:
+        query = "SELECT name, iso2 FROM countries LIMIT 50;"
+        data = execute_query(query)
+        return render_template('countries.html', countries=data)
+    except Exception as e:
+        return str(e)
 
 
-# search countries
+# -------------------------
+# SEARCH COUNTRIES (BY CONTINENT OR NAME)
+# -------------------------
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
-        continent = request.form['continent']
+        keyword = request.form['keyword']
 
-        query = "SELECT Name, Population FROM Country WHERE Continent = %s;"
-        data = execute_query(query, (continent,))
+        try:
+            query = """
+            SELECT name, iso2
+            FROM countries
+            WHERE name LIKE %s
+            LIMIT 50;
+            """
+            data = execute_query(query, (f"%{keyword}%",))
+            return render_template('countries.html', countries=data)
 
-        return render_template('display_users.html', countries=data)
+        except Exception as e:
+            return str(e)
 
-    return render_template('add_user.html')  # reuse as search form
+    return render_template('search.html')
 
 
-# joins
-@app.route('/cities', methods=['GET', 'POST'])
-def cities():
+# -------------------------
+# FIND CITIES BY COUNTRY CODE
+# -------------------------
+@app.route('/find', methods=['GET', 'POST'])
+def find():
     if request.method == 'POST':
-        country = request.form['country']
+        country_code = request.form['country_code']
 
-        query = """
-        SELECT City.Name, Country.Name AS Country
-        FROM City
-        JOIN Country ON City.CountryCode = Country.Code
-        WHERE Country.Name = %s;
-        """
-        data = execute_query(query, (country,))
+        try:
+            query = """
+            SELECT cities.name AS city, countries.name AS country
+            FROM cities
+            JOIN countries ON cities.country_id = countries.id
+            WHERE countries.iso2 = %s
+            LIMIT 100;
+            """
+            data = execute_query(query, (country_code,))
+            return render_template('find.html', results=data)
 
-        return render_template('display_users.html', countries=data)
+        except Exception as e:
+            return str(e)
 
-    return render_template('delete_user.html')  # reuse as city form
+    return render_template('find.html')
 
 
-
+# -------------------------
+# RUN APP
+# -------------------------
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
